73dd4a641b8653709b0f96ee11805e362e196f67
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2025-10-31 15:25:57 -0400

    Don't chunk on 304! Causes time outs with python libraries

--- https.go
+++ https.go
@@ -401,7 +401,9 @@ func (proxy *ProxyHttpServer) handleHttps(w http.ResponseWriter, r *http.Request
 						// Since we don't know the length of resp, return chunked encoded response
 						// TODO: use a more reasonable scheme
 						resp.Header.Del("Content-Length")
-						resp.Header.Set("Transfer-Encoding", "chunked")
+						if resp.StatusCode != 304 {
+							resp.Header.Set("Transfer-Encoding", "chunked")
+						}
 					}
 					// Force connection close otherwise chrome will keep CONNECT tunnel open forever
 					if !isWebsocket {
